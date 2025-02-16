cce18b669dd32ffb2279fc60260ac0bacd64903a
sohpha <slube042@uottawa.ca>
2025-02-16 12:46:23 -0500

    display booking details

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Handles booking logic
 
 window.onload = function () {
-    var service = ""
-    var subject = ""
-    var tutor = ""
-    var date = ""
-    var time = ""
-    var firstName = ""
-    var lastName = ""
-    var email = ""
-    var phoneNumber = ""
+    service = ""
+    subject = ""
+    tutor = ""
+    date = ""
+    time = ""
+    firstName = ""
+    lastName = ""
+    email = ""
 
     if (document.getElementById("subject")) {
         var subjectOption = document.getElementById("subject")
@@ -17,13 +16,11 @@ window.onload = function () {
             var tutors = document.getElementById("tutor")
             var idris = tutors.options[3]
             var victor = tutors.options[2]
-            if (subject.selectedIndex === 1 || subject.selectedIndex === 2) {
-
+            if (subjectOption.selectedIndex === 1 || subjectOption.selectedIndex === 2) {
                 victor.style.display = 'unset'
                 tutors.selectedIndex = 2
                 idris.style.display = 'none';
-            } else if (subject.selectedIndex === 3 || subject.selectedIndex === 4) {
-
+            } else if (subjectOption.selectedIndex === 3 || subjectOption.selectedIndex === 4) {
                 idris.style.display = 'unset'
                 tutors.selectedIndex = 3
                 victor.style.display = 'none';
@@ -37,41 +34,75 @@ window.onload = function () {
         step2Btn.onclick = function () {
             selected = document.getElementById("serviceType")
             service = selected.options[selected.selectedIndex].text;
-            
+
             selected = document.getElementById("subject")
             subject = selected.options[selected.selectedIndex].text;
 
             selected = document.getElementById("tutor")
             tutor = selected.options[selected.selectedIndex].text;
+
+            localStorage.setItem("service", service)
+            localStorage.setItem("subject", subject)
+            localStorage.setItem("tutor", tutor)
         }
 
-    } 
+    }
 
     if (document.getElementById("step3Btn")) {
-        date = document.getElementById("step3Btn").value
-        selected = document.getElementById("time")
-        time = selected.options[selected.selectedIndex].text;
+        step3Btn = document.getElementById("step3Btn") 
+        step3Btn.onclick = function() {
+            date = document.getElementById("date").value
+            selected = document.getElementById("time")
+            time = selected.options[selected.selectedIndex].text;
+
+            localStorage.setItem("date", date)
+            localStorage.setItem("time", time)
+
+        }
     }
 
     if (document.getElementById("bookSessionBtn")) {
         bookSessionBtn = document.getElementById("bookSessionBtn")
 
-        element = document.getElementById("firstName")
-        firstName = element.innerText
+        bookSessionBtn.onclick = function () {
+            var element = document.getElementById("firstName")
+            firstName = element.value
 
-        element = document.getElementById("lastName")
-        lastName = element.innerText
+            element = document.getElementById("lastName")
+            lastName = element.value
 
-        element = document.getElementById("emailAddress")
-        email = element.innerText
+            element = document.getElementById("emailAddress")
+            email = element.value
 
-        element = document.getElementById("phoneNumber")
-        phoneNumber = element.innerText
-
+            localStorage.setItem("fullName", firstName + " " + lastName)
+            localStorage.setItem("email", email)
+        }
     }
 
     if (document.getElementById("bookingConfirmation")) {
-        
+
+        console.log(localStorage)
+        fullName = document.getElementById("fullName")
+        fullName.innerHTML += localStorage.getItem("fullName")
+
+        sessionDate = document.getElementById("sessionDate")
+        sessionDate.innerHTML += localStorage.getItem("date")
+
+        sessionTime = document.getElementById("sessionTime")
+        sessionTime.innerHTML += localStorage.getItem("time")
+
+        sessionService = document.getElementById("sessionService")
+        sessionService.innerHTML += localStorage.getItem("service")
+
+        sessionSubject = document.getElementById("sessionSubject")
+        sessionSubject.innerHTML += localStorage.getItem("subject")
+
+        sessionTutor = document.getElementById("sessionTutor")
+        sessionTutor.innerHTML += localStorage.getItem("tutor")
+
+        emailConfirmation = document.getElementById("studentEmail")
+        emailConfirmation.innerHTML += localStorage.getItem("email")
+
     }
 
 
